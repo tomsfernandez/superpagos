ec0b5c6689e622acd50a99b4f08ddeab07909461
Tomás Fernández <tomas.fernandez@greencodesoftware.com>
2019-02-10 17:54:35 -0300

    deleted vue file

--- vue/src/validations.js
+++
@@ -1,7 +0,0 @@
-import spected from 'spected';
-
-/*export const validateFormData = data => {
-  const values = Object.values(spected(validationRules, data));
-  const errors = values.filter(x => x !== true);
-  return errors.flatMap(x => x)
-};*/
