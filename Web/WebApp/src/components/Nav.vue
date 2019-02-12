082c1837c7ac100004d7f0d7180e50a5c939ddcd
LedesmaBruno <bruno.ledesma96@gmail.com>
2019-02-12 14:26:46 -0300

    Logout cypress tests.

--- Web/WebApp/src/components/Nav.vue
+++ Web/WebApp/src/components/Nav.vue
@@ -1,6 +1,8 @@
 <template>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<a class="navbar-brand" href="#">Superpagos</a>
+		<router-link :to="{path: '/home'}">
+			<a class="navbar-brand">Superpagos</a>
+		</router-link>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
